e5a844df5f4fe0ca4fa2bde265116058a09f681c
LeviCastilho <mtlevi.castilho.cardoso@gmail.com>
2024-11-08 14:09:06 -0300

    Alterando o arquivo app.ts do src

---
+++ src/app.ts
@@ -0,0 +1,35 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import itemRoutes from './routes/itemRoutes';
+import categoriaRoutes from './routes/categoryRoutes';
+import fornecedorRoutes from './routes/supplierRoutes';
+import { db } from './models/db'; 
+
+
+dotenv.config();                       
+
+const app = express();
+
+
+app.use(express.json());
+
+// Rotas
+app.use('/item', itemRoutes); // Rotas para os itens
+app.use('/categoria', categoriaRoutes); // Rotas para as categorias
+app.use('/fornecedor', fornecedorRoutes); // Rotas para os fornecedores
+
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
+
+
+db.raw('SELECT 1')
+  .then(() => {
+    console.log('Conectado ao banco de dados com sucesso!');
+  })
+  .catch((err) => {
+    console.error('Erro ao conectar ao banco de dados:', err);
+  });
