31ee4d1c1fde5aa07ddb13d75f896eb53005908d
Jotinha14 <vitorjouro@gmail.com>
2024-11-27 21:14:09 -0300

    Alterando rota pegar Item por categoria com paginação

--- src/routes/itemRoutes.ts
+++ src/routes/itemRoutes.ts
@@ -7,7 +7,7 @@ const router = Router();
 
 router.get('/', itemController.getItems); // Pegar todos os itens
 router.get('/:id', itemController.getItemPorId); // Pegar item por ID
-router.get('/categoria/:id', itemController.getItemPorCategoria); 
+router.get('/categoria/:id', itemController.getItensPorCategoriaPaginado); // pegando item por categoria
 router.post('/', itemController.criandoItemController); // Criar novo item
 router.put('/:id', itemController.atualizarItemController); // Atualizar item por ID
 router.delete('/:id', itemController.deleteItemPorIdController); // Deletar item por ID
